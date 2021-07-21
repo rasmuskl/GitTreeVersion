b3180d417696e4ba3fd025f0ce0af2f81945eee3
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2021-07-21 21:20:43 +0200

    Change analyze to use file graph

--- GitTreeVersion/Commands/AnalyzeCommand.cs
+++ GitTreeVersion/Commands/AnalyzeCommand.cs
@@ -1,9 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.IO;
-using GitTreeVersion.Deployables;
+using System.Threading;
+using GitTreeVersion.Context;
+using Spectre.Console;
 
 namespace GitTreeVersion.Commands
 {
@@ -18,37 +19,38 @@ namespace GitTreeVersion.Commands
 
         private void Execute(string project)
         {
-            var analyzedProjects = new HashSet<string>();
-            var projectQueue = new Queue<string>();
-            projectQueue.Enqueue(project);
+            var projectPath = Path.GetFullPath(project);
+            var projectDirectoryPath = Path.GetDirectoryName(projectPath);
+            var tree = new Tree(Path.GetFileName(projectPath));
 
-            var processor = new CsprojDeployableProcessor();
-
-            while (projectQueue.TryDequeue(out var nextProject))
+            if (!File.Exists(projectPath))
             {
-                var fileInfo = new FileInfo(nextProject);
+                throw new Exception($"File not found: {project}");
+            }
 
-                if (!fileInfo.Exists)
-                {
-                    throw new Exception($"Unable to find project: {project}");
-                }
+            AnsiConsole
+                .Status()
+                .Spinner(Spinner.Known.Aesthetic)
+                .Start("Analyzing project...", ctx =>
+            {
+                var graph = ContextResolver.GetFileGraph(projectDirectoryPath);
+                AddDependencies(tree, graph, projectPath);
+            });
+            
+            AnsiConsole.Render(tree);
+        }
 
-                if (analyzedProjects.Contains(fileInfo.FullName))
-                {
-                    continue;
-                }
-                
-                Console.WriteLine($"Analyzing: {fileInfo.FullName}");
-                
-                analyzedProjects.Add(fileInfo.FullName);
+        private void AddDependencies(IHasTreeNodes tree, FileGraph graph, string projectPath)
+        {
+            if (!graph.DeployableFileDependencies.TryGetValue(projectPath, out var dependencies))
+            {
+                return;
+            }
 
-                var deployablePaths = processor.GetSourceReferencedDeployablePaths(fileInfo);
-
-                foreach (var deployablePath in deployablePaths)
-                {
-                    projectQueue.Enqueue(deployablePath);
-                }
-
+            foreach (var dependency in dependencies)
+            {
+                var node = tree.AddNode(Path.GetFileName(dependency));
+                AddDependencies(node, graph, dependency);
             }
         }
     }
